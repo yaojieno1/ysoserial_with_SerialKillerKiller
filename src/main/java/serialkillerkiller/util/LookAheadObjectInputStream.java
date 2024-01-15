462aec16dfb42398b029d811a6e1226de7bba645
闻道解惑 <yaojieno1@hotmail.com>
2024-01-15 16:50:52 +0800

    Add SerialKillerKiller

---
+++ src/main/java/serialkillerkiller/util/LookAheadObjectInputStream.java
@@ -0,0 +1,17 @@
+package serialkillerkiller.util;
+
+import java.io.*;
+
+public class LookAheadObjectInputStream extends ObjectInputStream {
+    public LookAheadObjectInputStream(InputStream inputStream)
+            throws IOException {
+        super(inputStream);
+    }
+    protected Class<?> resolveClass(ObjectStreamClass desc) throws IOException, ClassNotFoundException {
+        String name = desc.getName();
+        if (name.indexOf("InvokerTransformer") > 0 || name.indexOf("InterceptorMethodHandler") > 0) {
+            throw new InvalidClassException( "Unauthorized deserialization attempt", desc.getName());
+        }
+        return super.resolveClass(desc);
+    }
+}
